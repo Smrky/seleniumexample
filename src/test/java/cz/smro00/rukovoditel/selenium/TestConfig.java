32f6084016c870d4e51c009b15631a47e330e64c
Smrky <SmrcekOndra@seznam.cz>
2020-01-03 13:25:18 +0100

    Methods for searching and deleting projects by name

--- src/test/java/cz/smro00/rukovoditel/selenium/TestConfig.java
+++ src/test/java/cz/smro00/rukovoditel/selenium/TestConfig.java
@@ -9,14 +9,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestConfig {
     public ChromeDriver driver;
+    public WebDriverWait wait;
     final String URL = "https://digitalnizena.cz/rukovoditel/";
 
     boolean runOnTravis = false;
     boolean windows = false;
-
 
     @Before
     public void init() {
@@ -34,11 +36,13 @@ public class TestConfig {
         }
 //        ChromeDriverService service = new ChromeDriverService()
         driver = new ChromeDriver(cho);
+        wait = new WebDriverWait(driver, 1);
     }
+
 
     @After
     public void tearDown() {
-        driver.close();
+        //driver.close();
     }
 
     //Methods
@@ -57,10 +61,10 @@ public class TestConfig {
     }
 
     public void loginTest() {
-        //Given
+        //Given + When (user logs in to page)
         rukovoditelLogin("rukovoditel", "vse456ru");
 
-        //Then
+        //Then (check user is logged)
         Assert.assertEquals("Rukovoditel | Dashboard", driver.getTitle());
         Assert.assertTrue(driver.getTitle().contentEquals("Rukovoditel | Dashboard"));
 
@@ -69,5 +73,40 @@ public class TestConfig {
 
         WebElement usernameSpan = driver.findElement(By.cssSelector(".username"));
         Assert.assertEquals("System Administrator", usernameSpan.getText());
+    }
+
+    public void deleteProject(String projectName) {
+        searchProject(projectName);
+
+        WebElement deleteProjectIcon = driver.findElement(By.cssSelector(".table .fa-trash-o"));
+        deleteProjectIcon.click();
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".modal-body")));
+
+        WebElement confirmDeleteDiv = driver.findElement(By.cssSelector("#uniform-delete_confirm"));
+        confirmDeleteDiv.click();
+        WebElement submitDeleteProjectButton = driver.findElement(By.cssSelector(".btn-primary-modal-action"));
+        submitDeleteProjectButton.click();
+    }
+
+    public void searchProject(String projectName) {
+        WebElement projectsLink = driver.findElement(By.cssSelector(".fa-reorder"));
+        projectsLink.click();
+
+        WebElement filtersDropdown = driver.findElement(By.cssSelector(".portlet-filters-preview .fa-angle-down"));
+        filtersDropdown.click();
+
+        WebElement defaultFilterLink = driver.findElement(By.cssSelector(".portlet-title .caption .btn-group:nth-child(1) ul li:nth-child(1) a"));
+        defaultFilterLink.click();
+
+        WebElement projectSearchInput = driver.findElement(By.cssSelector("#entity_items_listing66_21_search_keywords"));
+        projectSearchInput.clear();
+        projectSearchInput.sendKeys(projectName);
+
+        WebElement projectSearchIcon = driver.findElement(By.cssSelector(".fa-search"));
+        projectSearchIcon.click();
+
+        WebElement searchTable = driver.findElement(By.cssSelector(".table"));
+        wait.until(ExpectedConditions.stalenessOf(searchTable));
     }
 }
