c721c4344cc85e074109bf40efcba61e1b6a7b27
Smrky <SmrcekOndra@seznam.cz>
2020-01-03 16:28:54 +0100

    Changes in deleting existing projects + using method in TC1

--- src/test/java/cz/smro00/rukovoditel/selenium/ProjectTest.java
+++ src/test/java/cz/smro00/rukovoditel/selenium/ProjectTest.java
@@ -27,16 +27,8 @@ public class ProjectTest extends TestConfig {
 
 
         //When
-        WebElement addProjectButton = driver.findElement(By.cssSelector(".btn-primary"));
-        addProjectButton.click();
+        createProject("");
 
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".modal-body")));
-
-        WebElement projectNameInput = driver.findElement(By.cssSelector("#fields_158"));
-        projectNameInput.clear();
-
-        WebElement submitProjectButton = driver.findElement(By.cssSelector(".btn-primary-modal-action"));
-        submitProjectButton.click();
 
         //Then
         WebElement projectNameErrorLabel = driver.findElement(By.cssSelector("#fields_158-error"));
@@ -49,50 +41,23 @@ public class ProjectTest extends TestConfig {
         loginTest();
 
         // delete all projects with prefix if such exist
-        searchProject("smro00");
+        searchProject(namePrefix);
 
         // if search is empty - table has only one cell with "No Records Found"
         List<WebElement> tableCells = driver.findElements(By.cssSelector(".table tbody tr td"));
-        if(tableCells.size() > 1) {
-            List<WebElement> tableProjectNames = driver.findElements(By.cssSelector(".table tbody tr .field-158-td"));
-            List<String> projectNames = new ArrayList<>();
-
-            for (WebElement tableProjectName : tableProjectNames) {
-                projectNames.add(tableProjectName.getText());
-            }
-
-            for (String projectName : projectNames) {
-                deleteProject(projectName);
-            }
+        if(tableCells.size() == 2) {
+                deleteProject(namePrefix);
+        }
+        if(tableCells.size() > 2){
+            deleteAllProjects(namePrefix);
         }
 
         //When
-        WebElement addProjectButton = driver.findElement(By.cssSelector(".btn-primary"));
-        addProjectButton.click();
-
-
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".modal-body")));
-
-        WebElement projectPrioritySelectElement = driver.findElement(By.cssSelector("#fields_156"));
-        Select projectPrioritySelect = new Select(projectPrioritySelectElement);
-        projectPrioritySelect.selectByValue("35");
-
-        WebElement projectStatusSelectElement = driver.findElement(By.cssSelector("#fields_157"));
-        Select projectStatusSelect = new Select(projectStatusSelectElement);
-        projectStatusSelect.selectByValue("37");
-
         String uuid = UUID.randomUUID().toString();
-        String projectName = "smro00-"+uuid;
-        WebElement projectNameInput = driver.findElement(By.cssSelector("#fields_158"));
-        projectNameInput.sendKeys(projectName);
-
-        WebElement projectDateInput = driver.findElement(By.cssSelector("#fields_159"));
+        String projectName = namePrefix+"-"+uuid;
         Date date = new Date();
-        SimpleDateFormat dashFormat = new SimpleDateFormat("yyyy-MM-dd");
-        projectDateInput.sendKeys(dashFormat.format(date));
 
-        WebElement submitProjectButton = driver.findElement(By.cssSelector(".btn-primary-modal-action"));
-        submitProjectButton.click();
+        createProject(projectName);
 
         //Then
         Assert.assertEquals("Rukovoditel | Tasks", driver.getTitle());
